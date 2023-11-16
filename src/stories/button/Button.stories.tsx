4e562667d4aaffd40f8f3588b7ccbbe3569fa067
Dario Puddinu <dario.puddinu@abinsula.com>
2023-11-16 17:20:30 +0100

    button story complete

--- src/stories/button/Button.stories.tsx
+++ src/stories/button/Button.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
+import { ButtonVariants } from './ButtonVariants';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof Button> = {
@@ -7,23 +8,26 @@ const meta: Meta<typeof Button> = {
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'centered',
+    layout: 'centered'
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  // argTypes: {
-  //   intent
-  // },
-
-}
+  argTypes: {
+    intent: ButtonVariants
+  }
+};
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
-  render: () => {
-    return <Button>default</Button>
+  render: (args) => {
+    return (
+      <Button intent={args.intent} size={args.size}>
+        Click me!
+      </Button>
+    );
   }
 };
--- src/stories/button/ButtonVariants.tsx
+++ src/stories/button/ButtonVariants.tsx
@@ -1,16 +1,32 @@
 import { cva } from 'cva';
 
-export const ButtonVariants = cva(['btn w-20 h-10 '], {
+export const ButtonVariants = cva(['btn'], {
   variants: {
     intent: {
       primary: ['btn-primary'],
-      secondary: ['btn-secondary']
+      secondary: ['btn-secondary'],
+      neutral: ['btn-neutral'],
+      accent: 'btn-accent',
+      info: 'btn-info',
+      warning: 'btn-warning',
+      error: 'btn-error',
+      ghost: 'btn-ghost',
+      link: 'btn-link',
+      outline: 'btn-outline',
+      active: 'btn-active',
+      disabled: 'btn-disabled',
+      glass: 'glass',
+      success: 'btn-success'
     },
     size: {
-      xs: ['btn-xs'],
-      sm: ['btn-sm'],
-      md: ['btn-md'],
-      lg: ['btn-lg']
+      xs: 'btn-xs',
+      sm: 'btn-sm',
+      md: 'btn-md',
+      lg: 'btn-lg',
+      wide: 'btn-wide',
+      block: 'btn-block',
+      circle: 'btn-circle',
+      square: 'btn-square'
     }
   },
   compoundVariants: [
